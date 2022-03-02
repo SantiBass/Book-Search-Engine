3b426cebd9c0bcc95383c2451667eda902067049
SantiBass <sibarraryan@gmail.com>
2022-03-02 11:49:52 -0800

    creatting schemas

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
+const db = require('config/connections');
+const {typeDefs, resolvers} = require('./');
 const app = express();
+const {authMiddleware} = require('./utils/auth')
 const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
