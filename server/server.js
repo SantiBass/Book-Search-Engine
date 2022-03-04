d69f0970f420d53f9afdba62056b303b333d024e
SantiBass <sibarraryan@gmail.com>
2022-03-04 14:04:25 -0800

    againg working on login/signup page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
@@ -20,7 +22,7 @@ const startServer = async () => {
 
 startServer()
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
