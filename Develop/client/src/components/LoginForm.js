52d3e919b97b6ba30f6e228337c7bbdaddfc36cd
SantiBass <sibarraryan@gmail.com>
2022-03-03 16:04:29 -0800

    working on the loginform.js

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,8 +1,9 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -10,6 +11,13 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  useEffect(() =>{
+    if(error){
+      setShowAlert(true);
+    }else{
+      setShowAlert(false)
+    }
+  }, [error]);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
